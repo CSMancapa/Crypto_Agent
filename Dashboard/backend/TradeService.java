cdf375567793bc8f1204a95563ef134b2a2f00eb
CebolakheSandiso <cebolakhe@softlytic.io>
2025-08-18 00:29:38 +0200

    Latest changes

--- Dashboard/backend/TradeService.java
+++
@@ -1,8 +0,0 @@
-@Service
-public class TradeService {
-    
-    @Scheduled(fixedRate = 5000) // Update every 5 sec
-    public void updateTrades() {
-        // Optional: Add live trade processing
-    }
-}
